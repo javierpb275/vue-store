7380bcd7984d01e498af49214549182226b517ee
javierpb275 <javierpb275@gmail.com>
2021-04-11 13:15:45 +0200

    created components productitem and productlist. worked in the productlist and added productlist to the app.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-
+    <product-list></product-list>
   </div>
 </template>
 
 <script>
+import ProductList from '@/components/ProductList';
 
 export default {
   name: 'App',
+  components: {
+    ProductList
+  }
 }
 </script>
 
---
+++ src/components/ProductList.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    {{products.length}}
+</div>
+    
+</template>
+
+<script>
+import {mapActions, mapMutations, mapState} from 'vuex';
+
+export default {
+    //get products
+    mounted() {
+        this.fetchProducts();
+    },
+    //configure pagination
+    data() {
+        return {
+            paginate: ['products'],//paginate is a required key that has to be an array and it gives name to the pagination (products). It is simply an alias
+            perPage: 3 //registries that we wanna show per page
+        }
+    },
+    //access products that the http query returns
+    computed: {
+        //(module, [stateKey])
+        ...mapState('products', ['products'])
+    },
+    //map the action in order to establish the products
+    methods: {
+        //(module, [action])
+        ...mapActions('products', ['fetchProducts'])
+    }
+}
+</script>
