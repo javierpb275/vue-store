e862ab6af10aa9ccb03aa5eb7b68a6e962b6b61c
javierpb275 <javierpb275@gmail.com>
2021-04-11 16:59:06 +0200

    created shoplayout component. using shoplayout component in App in order to improve the layout of our products

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="app">
-    <product-list></product-list>
+  <div id="app" class="mt-5">
+    <shop-layout>
+      <template slot="product-list">
+      <product-list></product-list>
+      </template>
+      <template slot="cart">
+        Cart
+      </template>
+    </shop-layout>
   </div>
 </template>
 
 <script>
+import ShopLayout from '@/components/ShopLayout';
 import ProductList from '@/components/ProductList';
 
 export default {
   name: 'App',
   components: {
+    ShopLayout,
     ProductList
   }
 }
---
+++ src/components/ShopLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col cols="9">
+                <slot name="product-list"></slot>
+            </b-col>
+            <b-col cols="3">
+                <slot name="cart"></slot>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
