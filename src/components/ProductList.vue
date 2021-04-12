0dfaac00cbc45e55f049d64df757782dbc516eb8
javier-pardinas <javierpb275@gmail.com>
2021-04-12 10:28:27 +0200

    adding products to cart

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -44,8 +44,9 @@ export default {
     methods: {
         //(module, [action])
         ...mapActions('products', ['fetchProducts']),
+        ...mapMutations('cart', ['addProduct']),
         addProductToCart(product) {
-            console.log(product);
+            this.addProduct(product);
         }
     }
 }
