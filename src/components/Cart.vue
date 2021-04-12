fe43a9697e4fbdd7243052648e47916e8dedcfbd
javier-pardinas <javierpb275@gmail.com>
2021-04-12 10:45:02 +0200

    created cart component  and defined logic

---
+++ src/components/Cart.vue
@@ -0,0 +1,25 @@
+<template>
+    
+</template>
+
+<script>
+import {mapGetters, mapMutations, mapState} from 'vuex';
+
+export default {
+    data() {
+        return {
+            fields: ['name', 'qty', 'price', 'actions']//fields: definition of columns
+        }
+    },
+    computed: {
+        //(module, [state])
+        ...mapState('cart', ['cart']),
+        //(module, [getter])
+        ...mapGetters('cart', ['totalCost']),
+    },
+    methods: {
+        //(module, [mutation])
+      ...mapMutations('cart', ['removeProductFromCart'])  
+    }
+}
+</script>
